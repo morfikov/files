283163ef7a475b587e3a96d3a2355bd49e581f72
Mikhail Morfikov <mmorfikov@gmail.com>
2015-04-12 10:18:43 +0200

    checksum

--- scripts/ff-tb-updater.sh
+++ scripts/ff-tb-updater.sh
@@ -22,7 +22,7 @@ update() {
 	update_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[0-9]*\.[0-9]*\.[0-9]*\.partial\.mar" | sort -u)"
 	new_version="$(echo $update_file | sort -u | cut -d '-' -f 3 |cut -d '.' -f 1-3)"
 	signature_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[0-9]*\.[0-9]*\.[0-9]*\.partial\.mar.asc" | sort -u)"
-	
+
 	mkdir -p $tmp_dir
 	
 	echo -en "Installed version \033[01;32m$installed_verion\033[0m, "
@@ -38,6 +38,22 @@ update() {
 	wget -q --show-progress -O $tmp_dir/update.mar $url/$update_file
 	wget -q --show-progress -O $tmp_dir/update.mar.asc $url/$signature_file
 
+	echo -en "Running checksum... "
+	
+	sum_update_file="$(curl -s http://download-origin.cdn.mozilla.net/pub/mozilla.org/$app/releases/latest/SHA1SUMS | egrep "linux-`uname -m`/$lang/$update_file" | egrep -e ".mar$" | cut -d ' ' -f 1)"
+	sum_signature_file="$(curl -s http://download-origin.cdn.mozilla.net/pub/mozilla.org/$app/releases/latest/SHA1SUMS | egrep "linux-`uname -m`/$lang/$signature_file" | egrep -e ".asc$" | cut -d ' ' -f 1)"
+
+	sum_downloaded_update_file="$(sha1sum $tmp_dir/update.mar | cut -d ' ' -f 1)"
+	sum_downloaded_signature_file="$(sha1sum $tmp_dir/update.mar.asc | cut -d ' ' -f 1)"
+
+	if [ "$sum_update_file" == "$sum_downloaded_update_file" ] && [ "$sum_signature_file" == "$sum_downloaded_signature_file" ]; then
+		echo -e "\033[01;32mpassed\033[0m!"
+	else
+		echo -e "\033[01;31merror\033[0m, file corruption detected, try running this script again!"
+		exit 1
+	fi
+
+	echo -en "Running file sygnature verification... "
 	signature_verification="$(gpg --verify $tmp_dir/update.mar.asc 2>&1)"
 
 	no_key="$(echo $signature_verification | egrep "public key not found")"
@@ -51,10 +67,10 @@ update() {
 	good_signature="$(echo $signature_verification | egrep "Good signature")"
 
 	if [ -z "$good_signature" ]; then
-		echo -e "\033[01;31mBad signature\033[0m, file not verified, aborting..."
-		exit 0
+		echo -e "\033[01;31mbad signature\033[0m, file can't be verified, aborting..."
+		exit 1
 	else
-		echo -e "\033[01;32mGood signature\033[0m, file verified!"
+		echo -e "\033[01;32mgood signature\033[0m, file verified!"
 	fi
 	
 	cd $instalation_dir
