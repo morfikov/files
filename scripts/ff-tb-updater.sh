6117b198aca98bd8392ffb4ebf49ead13a59f4c8
Mikhail Morfikov <mmorfikov@gmail.com>
2015-10-17 18:25:35 +0200

    update: ff-tb-updater.sh

--- scripts/ff-tb-updater.sh
+++ scripts/ff-tb-updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Last modified: 2015.09.20
-# Version: 0.2.2
+# Last modified: 2015.10.02
+# Version: 0.2.3
 # Source: https://github.com/morfikov/files/blob/master/scripts/ff-tb-updater.sh
 # Author: Mikhail Morfikov <mmorfikov[at]gmail.com>
 # Copyright: 2015 Mikhail Morfikov <mmorfikov[at]gmail.com>
@@ -40,7 +40,7 @@ update() {
 	update_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[\.0-9]*partial\.mar" | sort -u)"
 	new_version="$(echo $update_file | sort -u | cut -d '-' -f 3 |cut -d '.' -f 1-3)"
 #	signature_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[0-9]*\.[0-9]*\.[0-9]*\.partial\.mar.asc" | sort -u)"
-	signature_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[\.0-9]*partial\.mar.asc" | sort -u)"
+##	signature_file="$(curl -s $url | egrep -o -e "$app\-$installed_verion\-[\.0-9]*partial\.mar.asc" | sort -u)"
 
 	mkdir -p $tmp_dir
 	
@@ -55,42 +55,43 @@ update() {
 	fi
 	
 	wget -q --show-progress -O $tmp_dir/update.mar $url/$update_file
-	wget -q --show-progress -O $tmp_dir/update.mar.asc $url/$signature_file
+##	wget -q --show-progress -O $tmp_dir/update.mar.asc $url/$signature_file
 
 	echo -en "Running checksum... "
 	
 	sum_update_file="$(curl -s http://download-origin.cdn.mozilla.net/pub/mozilla.org/$app/releases/latest/SHA1SUMS | egrep "linux-`uname -m`/$lang/$update_file" | egrep -e ".mar$" | cut -d ' ' -f 1)"
-	sum_signature_file="$(curl -s http://download-origin.cdn.mozilla.net/pub/mozilla.org/$app/releases/latest/SHA1SUMS | egrep "linux-`uname -m`/$lang/$signature_file" | egrep -e ".asc$" | cut -d ' ' -f 1)"
+##	sum_signature_file="$(curl -s http://download-origin.cdn.mozilla.net/pub/mozilla.org/$app/releases/latest/SHA1SUMS | egrep "linux-`uname -m`/$lang/$signature_file" | egrep -e ".asc$" | cut -d ' ' -f 1)"
 
 	sum_downloaded_update_file="$(sha1sum $tmp_dir/update.mar | cut -d ' ' -f 1)"
-	sum_downloaded_signature_file="$(sha1sum $tmp_dir/update.mar.asc | cut -d ' ' -f 1)"
+##	sum_downloaded_signature_file="$(sha1sum $tmp_dir/update.mar.asc | cut -d ' ' -f 1)"
 
-	if [ "$sum_update_file" == "$sum_downloaded_update_file" ] && [ "$sum_signature_file" == "$sum_downloaded_signature_file" ]; then
+##	if [ "$sum_update_file" == "$sum_downloaded_update_file" ] && [ "$sum_signature_file" == "$sum_downloaded_signature_file" ]; then
+	if [ "$sum_update_file" == "$sum_downloaded_update_file" ] ; then
 		echo -e "\033[01;32mpassed\033[0m!"
 	else
 		echo -e "\033[01;31merror\033[0m, file corruption detected, try running this script again!"
 		exit 1
 	fi
 
-	echo -en "Running file signature verification... "
-	signature_verification="$(gpg --verify $tmp_dir/update.mar.asc 2>&1)"
+##	echo -en "Running file signature verification... "
+##	signature_verification="$(gpg --verify $tmp_dir/update.mar.asc 2>&1)"
 
-	no_key="$(echo "$signature_verification" | egrep "public key not found")"
+##	no_key="$(echo "$signature_verification" | egrep "public key not found")"
 
-	if [ ! -z "$no_key" ]; then
-		signature_key="$(echo "$signature_verification" | egrep "using RSA key" | cut -d ' ' -f 20)"
-		gpg --recv-keys $signature_key > /dev/null 2>&1
-		signature_verification="$(gpg --verify $tmp_dir/update.mar.asc 2>&1)"
-	fi
+##	if [ ! -z "$no_key" ]; then
+##		signature_key="$(echo "$signature_verification" | egrep "using RSA key" | cut -d ' ' -f 20)"
+##		gpg --recv-keys $signature_key > /dev/null 2>&1
+##		signature_verification="$(gpg --verify $tmp_dir/update.mar.asc 2>&1)"
+##	fi
 
-	good_signature="$(echo "$signature_verification" | egrep "Good signature")"
+##	good_signature="$(echo "$signature_verification" | egrep "Good signature")"
 
-	if [ -z "$good_signature" ]; then
-		echo -e "\033[01;31mbad signature\033[0m, file can't be verified, aborting..."
-		exit 1
-	else
-		echo -e "\033[01;32mgood signature\033[0m, file verified!"
-	fi
+##	if [ -z "$good_signature" ]; then
+##		echo -e "\033[01;31mbad signature\033[0m, file can't be verified, aborting..."
+##		exit 1
+##	else
+##		echo -e "\033[01;32mgood signature\033[0m, file verified!"
+##	fi
 	
 	cd $instalation_dir
 	cp $instalation_dir/updater $tmp_dir
