bc45eecfe9fe794254f8b3e25942bc589a923fff
Mikhail Morfikov <mmorfikov@gmail.com>
2017-01-02 16:50:01 +0100

    update: lte script

--- scripts/smstools-lte-script.sh
+++ scripts/smstools-lte-script.sh
@@ -4,62 +4,66 @@ if [ $1 != "RECEIVED" ]; then
         exit 0
 fi
 
-# play 753924
 NADAWCA=$(awk '/^From:/ {print $2}' $2)
 ODBIORCA="48600123456"
 WIADOMOSC=$(sed '13!d' $2)
 
 lte=$(grep LTE $2)
-on=$(grep wlaczona $2)
-off=$(grep wylaczona $2)
+on=$(grep "zostala wlaczona" $2)
+off=$(grep "zostala wylaczona" $2)
+
 
 WAN_IF="wwan0"
 MODEM_IF="/dev/ttyUSB0"
 
 echo "New Message from: $NADAWCA , msg: $WIADOMOSC"
 
-if [ ! -z "$NADAWCA" ] && [ ! -z "$lte" ] && [ ! -z "$off" ] ; then
-	iptables -I INPUT -i $WAN_IF -j DROP
-	iptables -I FORWARD -o $WAN_IF -j DROP
-	iptables -I FORWARD -i $WAN_IF -j DROP
-	iptables -I OUTPUT -o $WAN_IF -j DROP
+if [ ! -z "$lte" ] && [ ! -z "$off" ] ; then
 
-	echo -e "AT+CMGF=1;^CURC=0;^USSDMODE=0\r" > $MODEM_IF
-	sleep 1
-	echo -e "AT+CUSD=1,\"*111*480*1#\",15\r" > $MODEM_IF
+        ifdown lte
+#       ip link set dev $WAN_IF down
 
-	echo "To: $ODBIORCA" > /tmp/sms
-	echo "" >> /tmp/sms
-	echo "Promocja Darmowy Internet LTE zostala wylaczona. Zakladanie blokady na iptables." >> /tmp/sms
+#		iptables -I INPUT -i $WAN_IF -j DROP
+#		iptables -I FORWARD -o $WAN_IF -j DROP
+#		iptables -I FORWARD -i $WAN_IF -j DROP
+#		iptables -I OUTPUT -o $WAN_IF -j DROP
 
-	mv /tmp/sms /var/spool/sms/outgoing/
+        echo "To: $ODBIORCA" > /tmp/sms
+        echo "" >> /tmp/sms
+        echo "Promocja Darmowy Internet LTE zostala wylaczona. Blokowanie internetu..." >> /tmp/sms
 
-#	ifdown wan
-#	ip link set dev $WAN_IF down
+        mv /tmp/sms /var/spool/sms/outgoing/
 
-	exit 0
+		echo -e "AT^SYSCFGEX=\"030201\",3FFFFFFF,1,2,800C5,,\r" > $MODEM_IF
+		echo -e "AT+CMGF=1;^CURC=0;^USSDMODE=0\r" > $MODEM_IF
+		echo -e "AT+CUSD=1,\"*111*480*1#\"\r" > $MODEM_IF
+#		echo -e "AT+CUSD=1,\"*111*480*1#\",15\r" > $MODEM_IF
 
-elif [ ! -z "$NADAWCA" ] && [ ! -z "$lte" ] && [ ! -z "$on" ] ; then
-	iptables -D INPUT -i $WAN_IF -j DROP
-	iptables -D FORWARD -o $WAN_IF -j DROP
-	iptables -D FORWARD -i $WAN_IF -j DROP
-	iptables -D OUTPUT -o $WAN_IF -j DROP
+        exit 0
 
-	echo "To: $ODBIORCA" > /tmp/sms
-	echo "" >> /tmp/sms
-	echo "Promocja Darmowy Internet LTE zostala ponownie wlaczona. Sciaganie blokady iptables." >> /tmp/sms
+elif [ ! -z "$lte" ] && [ ! -z "$on" ] ; then
 
-	mv /tmp/sms /var/spool/sms/outgoing/
+        ifup lte
+#       ip link set dev $WAN_IF up
 
-#	ifup wan
-#	ip link set dev $WAN_IF up
+#		iptables -D INPUT -i $WAN_IF -j DROP
+#		iptables -D FORWARD -o $WAN_IF -j DROP
+#		iptables -D FORWARD -i $WAN_IF -j DROP
+#		iptables -D OUTPUT -o $WAN_IF -j DROP
 
-	exit 0
+        echo "To: $ODBIORCA" > /tmp/sms
+        echo "" >> /tmp/sms
+        echo "Promocja Darmowy Internet LTE zostala ponownie wlaczona. Przywracanie dostepu do internetu..." >> /tmp/sms
+
+        mv /tmp/sms /var/spool/sms/outgoing/
+
+        exit 0
 
 else
+
 	echo "To: $ODBIORCA" > /tmp/sms
 	echo "" >> /tmp/sms
-	echo "nr: $NADAWCA\nmsg: $WIADOMOSC" >> /tmp/sms
+	echo "nr: $NADAWCA msg: $WIADOMOSC" >> /tmp/sms
 
 	mv /tmp/sms /var/spool/sms/outgoing
 
