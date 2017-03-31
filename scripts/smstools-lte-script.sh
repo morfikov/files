f73ef88aa922da79efc45ee7cd3dbc0658f5f48a
Mikhail Morfikov <mmorfikov@gmail.com>
2017-03-31 08:57:41 +0200

    update lte script

--- scripts/smstools-lte-script.sh
+++ scripts/smstools-lte-script.sh
@@ -13,59 +13,52 @@ on=$(grep "zostala wlaczona" $2)
 off=$(grep "zostala wylaczona" $2)
 
 
-WAN_IF="wwan0"
+LTE_IF="lte"
 MODEM_IF="/dev/ttyUSB0"
 
 echo "New Message from: $NADAWCA , msg: $WIADOMOSC"
 
 if [ ! -z "$lte" ] && [ ! -z "$off" ] ; then
 
-        ifdown lte
-#       ip link set dev $WAN_IF down
+    ifdown $LTE_IF
+    sleep 5
 
-#		iptables -I INPUT -i $WAN_IF -j DROP
-#		iptables -I FORWARD -o $WAN_IF -j DROP
-#		iptables -I FORWARD -i $WAN_IF -j DROP
-#		iptables -I OUTPUT -o $WAN_IF -j DROP
+    echo "To: $ODBIORCA" > /tmp/sms
+    echo "" >> /tmp/sms
+    echo "Promocja Darmowy Internet LTE zostala wylaczona. Blokowanie internetu..." >> /tmp/sms
 
-        echo "To: $ODBIORCA" > /tmp/sms
-        echo "" >> /tmp/sms
-        echo "Promocja Darmowy Internet LTE zostala wylaczona. Blokowanie internetu..." >> /tmp/sms
+    mv /tmp/sms /var/spool/sms/outgoing/
 
-        mv /tmp/sms /var/spool/sms/outgoing/
+    echo -e "AT^SYSCFGEX=\"030201\",3FFFFFFF,1,2,800C5,,\r" > $MODEM_IF
+    sleep 1
+    echo -e "AT+CMGF=1;^CURC=0;^USSDMODE=0\r" > $MODEM_IF
+    sleep 1
+    echo -e "AT+CUSD=1,\"*111*480*1#\"\r" > $MODEM_IF
+#   echo -e "AT+CUSD=1,\"*111*480*1#\",15\r" > $MODEM_IF
+    sleep 5
+    echo -e "AT+CUSD=1,\"0\"\r" > $MODEM_IF
 
-		echo -e "AT^SYSCFGEX=\"030201\",3FFFFFFF,1,2,800C5,,\r" > $MODEM_IF
-		echo -e "AT+CMGF=1;^CURC=0;^USSDMODE=0\r" > $MODEM_IF
-		echo -e "AT+CUSD=1,\"*111*480*1#\"\r" > $MODEM_IF
-#		echo -e "AT+CUSD=1,\"*111*480*1#\",15\r" > $MODEM_IF
-
-        exit 0
+    exit 0
 
 elif [ ! -z "$lte" ] && [ ! -z "$on" ] ; then
 
-        ifup lte
-#       ip link set dev $WAN_IF up
+    ifup $LTE_IF
 
-#		iptables -D INPUT -i $WAN_IF -j DROP
-#		iptables -D FORWARD -o $WAN_IF -j DROP
-#		iptables -D FORWARD -i $WAN_IF -j DROP
-#		iptables -D OUTPUT -o $WAN_IF -j DROP
+    echo "To: $ODBIORCA" > /tmp/sms
+    echo "" >> /tmp/sms
+    echo "Promocja Darmowy Internet LTE zostala ponownie wlaczona. Przywracanie dostepu do internetu..." >> /tmp/sms
 
-        echo "To: $ODBIORCA" > /tmp/sms
-        echo "" >> /tmp/sms
-        echo "Promocja Darmowy Internet LTE zostala ponownie wlaczona. Przywracanie dostepu do internetu..." >> /tmp/sms
+    mv /tmp/sms /var/spool/sms/outgoing/
 
-        mv /tmp/sms /var/spool/sms/outgoing/
-
-        exit 0
+    exit 0
 
 else
 
-	echo "To: $ODBIORCA" > /tmp/sms
-	echo "" >> /tmp/sms
-	echo "nr: $NADAWCA msg: $WIADOMOSC" >> /tmp/sms
+    echo "To: $ODBIORCA" > /tmp/sms
+    echo "" >> /tmp/sms
+    echo "nr: $NADAWCA msg: $WIADOMOSC" >> /tmp/sms
 
-	mv /tmp/sms /var/spool/sms/outgoing
+    mv /tmp/sms /var/spool/sms/outgoing
 
 fi
 
